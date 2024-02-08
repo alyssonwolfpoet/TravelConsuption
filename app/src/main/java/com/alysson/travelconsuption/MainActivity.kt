e07bf0fddbc3eb359a322b0a020a81e67d6e25d6
Alysson <alyssos@hotmail.com>
2024-02-08 00:32:49 -0300

    travelconsuption

---
+++ app/src/main/java/com/alysson/travelconsuption/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.alysson.travelconsuption
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.alysson.travelconsuption.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottonCalculate.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.botton_Calculate) {
+            calculate()
+        }
+    }
+
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomy.text.toString() != "")
+    }
+
+    private fun calculate() {
+        //toast notification
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+            if (autonomy == 0f) {
+                Toast.makeText(this, R.string.no_div_zero, Toast.LENGTH_SHORT).show()
+                binding.textLabelResul.text = "R$ 0"
+            } else {
+                val totalValue = (price * distance) / autonomy
+
+                //val totalValueStr = "R$ ${"%.2f".format(totalValue)}"
+                binding.textLabelResul.text = "R$ ${"%.2f".format(totalValue)}"
+            }
+        } else {
+            Toast.makeText(this, R.string.Validation_fill_all_fields, Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+}
+
+
